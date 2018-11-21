98a0b84aa14068f2e550fc99c980639d1fcfc99c
RR1998 <00259517@uca.edu.sv>
2018-11-20 22:47:21 -0600

    fgagsg

--- DBWEB.js
+++ DBWEB.js
@@ -1,6 +1,6 @@
 db.Createcollection("Usuarios", {validator: { $and:
         [
-            {User_ID: {$type: "Object ID"}},
+            {User_ID: {$type: "Integer"}},
             {Nombres: {$type: "String"}},
             {Apellidos: {$type: "String"}},
             {FechaNacimiento: {$type: "Date"}},
@@ -13,7 +13,8 @@ db.Createcollection("Usuarios", {validator: { $and:
 
 db.Createcollection("Documentos", {validator: { $and:
         [
-            {Documentos_ID: {$type: "Object ID"}},
+            {Documentos_ID: {$type: "Integer"}},
+            {User_Creador: {$type: "Integer"}},
             {Album: {$type: "Integer", default: 0}},
             {Document: {$type: "String"}},
             {FechaCreacion: {$type: "Timestamp"}},
